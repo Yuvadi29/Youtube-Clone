240a6db3a45e6cb11bd5b4da269d920ce91a0c68
[Aditya,Trivedi] <[adityatrivedi612@gmail.com]>
2022-11-23 16:58:49 +0530

    Routing of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000000" }}>
+            <Navbar/>
+            <Routes>
+                <Route path="/" exact element={<Feed/>}></Route>
+                <Route path="/video/:id" element={<VideoInfo/>}></Route>
+                <Route path="/channel/:id" element={<ChannelInfo/>}></Route>
+                <Route path="/search/:searchTerm" element={<Search/>}></Route>
+            </Routes>
+        </Box>
+    </BrowserRouter>
 }
 
 export default App
