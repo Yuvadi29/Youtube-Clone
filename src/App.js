8cdd849c546f223592ad321d31f3c8dc17f09bb9
[Aditya,Trivedi] <[adityatrivedi612@gmail.com]>
2022-11-23 17:12:32 +0530

    Creation of components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+
+//Routing Essentials
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Box } from '@mui/material';
 
-const App = () => {
+// Importing the components
+import Navbar from './components/Navbar';
+import Feed from './components/Feed';
+import VideoInfo from './components/VideoInfo';
+import ChannelInfo from './components/ChannelInfo';
+import Search from './components/Search';
+
+const App = () => (
     <BrowserRouter>
-        <Box sx={{ backgroundColor: "#000000" }}>
-            <Navbar/>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
             <Routes>
-                <Route path="/" exact element={<Feed/>}></Route>
-                <Route path="/video/:id" element={<VideoInfo/>}></Route>
-                <Route path="/channel/:id" element={<ChannelInfo/>}></Route>
-                <Route path="/search/:searchTerm" element={<Search/>}></Route>
+                <Route path="/" exact element={<Feed />}></Route>
+                <Route path="/video/:id" element={<VideoInfo />}></Route>
+                <Route path="/channel/:id" element={<ChannelInfo />}></Route>
+                <Route path="/search/:searchTerm" element={<Search />}></Route>
             </Routes>
         </Box>
     </BrowserRouter>
-}
+);
 
-export default App
+export default App;
