1fff52ed5cbb79799accd142c204996d08685789
Yuvadi29 <adityatrivedi612@gmail.com>
2022-11-25 12:56:35 +0530

    Video Card Added

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { demoThumbnailUrl, demoVideoUrl, demoChannelUrl, demoVideoTitle, demoChannelTitle } from '../utilities/constant';
 
-const VideoCard = () => {
+const VideoCard = ({ video: { id: {
+  videoId }, snippet } }) => {
+  // console.log(videoId, snippet);
   return (
-    <div>VideoCard</div>
+    <Card sx={{ width: { sm: '358px', md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: '12px' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+
+        <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' fontWeight="bold" color='#FFF'>
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+
+        <Link to={snippet?.channelId ? `channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontWeight="light" color='#d3d3d3'>
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: '12px', color: 'grey', marginLeft: '5px', marginTop: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -7,10 +7,10 @@ const Videos = ({ videos }) => {
   console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => (
-        <Box key={index}>
-          {item.id.videoID && <VideoCard video={item} />}
-          {item.id.channelID && <ChannelCard detail={item} />}
+      {videos.map((item, ind) => (
+        <Box key={ind}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard detail={item} />}
         </Box>
       ))}
     </Stack>
